16ed5c63bd190b639963058efb542f7ca1e408a7
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-05-03 12:04:11 +0300

    Add styling to NoMatch page

--- src/routes/NotMatch.js
+++ src/routes/NotMatch.js
@@ -1,6 +1,14 @@
 function NotMatch() {
   return (
-    <h2>There is no match for the entered address</h2>
+    <h2
+      style={{
+        padding: '10px 30px',
+        maxWidth: '768px',
+        margin: 'auto',
+      }}
+    >
+      There is no match for the entered address
+    </h2>
   );
 }
 
